1b1561cf44bcf5e2d163c11d25d35bfa137a68d1
Taz <97239140+taz-chiles@users.noreply.github.com>
2024-09-06 15:45:59 +0100

    Import sheets plugin

--- _config.ts
+++ _config.ts
@@ -5,6 +5,7 @@ import jsonLoader from "lume/core/loaders/json.ts";
 
 import oiLumeViz from "https://deno.land/x/oi_lume_viz@v0.16.0/mod.ts";
 import autoDependency from 'https://deno.land/x/oi_lume_utils@v0.4.0/processors/auto-dependency.ts';
+import sheets from "lume/plugins/sheets.ts";
 
 
 const site = lume({
@@ -16,6 +17,7 @@ site.process(['.html'], (pages) => pages.forEach(autoDependency));
 site.use(base_path());
 site.use(metas());
 site.use(oiLumeViz());
+site.use(sheets());
 
 site.loadData(['.geojson'], jsonLoader);
 
