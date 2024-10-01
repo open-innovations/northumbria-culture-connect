ab92bab7649e56c8eaf9fa020447359e86f76da3
Giles Dring <giles@dringtech.com>
2024-10-01 20:24:32 +0100

    Geocode landscape map

---
+++ pipelines/pipeline_utils/filesystem/paths.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+_ROOT = (Path(__file__).parent / '../../..').resolve()
+
+RAW_DATA = _ROOT / 'raw'
+
+DATA = _ROOT / 'data'
+
+REF_DATA = DATA / 'reference'
+
+RAW_DATA.mkdir(exist_ok=True, parents=True)
+DATA.mkdir(exist_ok=True, parents=True)
+REF_DATA.mkdir(exist_ok=True, parents=True)
