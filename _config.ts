3c3b1d93b423787f72a5b68dbc1a8cc6d26f3dc1
Giles Dring <giles@dringtech.com>
2024-10-03 14:41:03 +0100

    Move site styling to separate stylesheet

--- _config.ts
+++ _config.ts
@@ -1,12 +1,15 @@
 import lume from "lume/mod.ts";
+
+// Lume plugins
+import jsonLoader from "lume/core/loaders/json.ts";
 import base_path from "lume/plugins/base_path.ts";
 import metas from "lume/plugins/metas.ts";
-import jsonLoader from "lume/core/loaders/json.ts";
-
-import oiLumeViz from "https://deno.land/x/oi_lume_viz@v0.16.0/mod.ts";
-import autoDependency from 'https://deno.land/x/oi_lume_utils@v0.4.0/processors/auto-dependency.ts';
+import postcss from 'lume/plugins/postcss.ts';
 import sheets from "lume/plugins/sheets.ts";
 
+// OI plugins
+import autoDependency from 'https://deno.land/x/oi_lume_utils@v0.4.0/processors/auto-dependency.ts';
+import oiLumeViz from "https://deno.land/x/oi_lume_viz@v0.16.0/mod.ts";
 
 const site = lume({
     src: './src',
@@ -15,6 +18,7 @@ const site = lume({
 
 site.process(['.html'], (pages) => pages.forEach(autoDependency));
 site.use(base_path());
+site.use(postcss());
 site.use(metas());
 site.use(oiLumeViz());
 site.use(sheets());
