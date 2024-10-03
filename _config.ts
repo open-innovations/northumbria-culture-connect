1ced45a35a6ed89d5604bfc3060f3d1b771b42c4
Giles Dring <giles@dringtech.com>
2024-10-03 21:40:58 +0100

    Styling headline data

--- _config.ts
+++ _config.ts
@@ -30,4 +30,11 @@ site.use(sheets());
 
 site.loadData(['.geojson'], jsonLoader);
 
+site.filter('displayCurrency', (x: string) => parseInt(x)
+    .toLocaleString('en-GB', {
+        style: "currency",
+        currency: 'GBP',
+        maximumFractionDigits: 0
+    }));
+
 export default site;
