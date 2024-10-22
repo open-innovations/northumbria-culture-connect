18b24199258f376cd853d25dce7eedac671cc8f6
Giles Dring <giles@dringtech.com>
2024-10-22 12:37:23 +0100

    Move site header and footer styling into components

--- _config.ts
+++ _config.ts
@@ -21,7 +21,10 @@ import nesting from "npm:postcss-nesting";
 
 const site = lume({
     src: './src',
-    location: new URL('https://open-innovations.github.io/northumbria-culture-connect/')
+    location: new URL('https://open-innovations.github.io/northumbria-culture-connect/'),
+    components: {
+        cssFile: '_includes/css/_components.css'
+    }
 });
 site.data('LOCAL', site.options.location.hostname === 'localhost');
 
