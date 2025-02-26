9db7ca8b9a1f41e1589f0e04a02a6fb271ef4ffb
slowe <299787+slowe@users.noreply.github.com>
2025-02-26 18:20:51 +0000

    Update Likert colours

--- oi-viz-config.ts
+++ oi-viz-config.ts
@@ -20,12 +20,12 @@ export default {
 		"series": 
 			[ '#8F3E8D','#33A38C','#005837','#F25C29','#00478A', '#ED1163','#c79ec6','#99d1c5','#7fa3c4','#f8ad94','#7fab9b'],
 		"names":{
-			"StrongAgree": "#005837",
-			"Agree": "#33a38c",
-			"Neither": "#7fa3c4",
-			"Disagree": "#f24d8a",
-			"StrongDisagree": "#8f3e8d",
-			"DontKnow": "#999999"
+			"Likert5": "#005837",
+			"Likert4": "#33a38c",
+			"Likert3": "#bfd1e2",
+			"Likert2": "#f24d8a",
+			"Likert1": "#8f3e8d",
+			"Likert0": "#999999"
 		}
 	}
 };
